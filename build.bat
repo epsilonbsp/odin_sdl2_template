628f282fb3bbf766d249a6e219acc58139827e14
epsilonbsp <deltasampler@gmail.com>
2026-03-08 15:00:56 +0200

    update build scripts

--- build.bat
+++ build.bat
@@ -1,21 +1,23 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set BUILD_DIR=build
+set OUTPUT_DIR=%BUILD_DIR%\output
+set VENDOR_DIR=%BUILD_DIR%\vendor
+
 set EXE_NAME=odin_sdl2_template.exe
 set SDL_VERSION=release-2.32.10
 
-if not exist build mkdir build
-if not exist build\output mkdir build\output
-if not exist build\vendor mkdir build\vendor
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
+if not exist %VENDOR_DIR% mkdir %VENDOR_DIR%
 
 cls
 
 if "%~1" == "get-sdl2" (
-    curl -L -o build\output\SDL2.dll https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl2/SDL2.dll
-    curl -L -o build\output\SDL2.lib https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl2/SDL2.lib
-)
-
-if "%~1" == "build-sdl2" (
+    curl -L -o %OUTPUT_DIR%\SDL2.dll https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl2/SDL2.dll
+    curl -L -o %OUTPUT_DIR%\SDL2.lib https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl2/SDL2.lib
+) else if "%~1" == "build-sdl2" (
     for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do set VS=%%i
 
     if "!VS!" equ "" (
@@ -23,7 +25,7 @@ if "%~1" == "build-sdl2" (
         exit /b 1
     )
 
-    cd build\vendor
+    cd %VENDOR_DIR%
 
     if not exist sld (
         git clone https://github.com/libsdl-org/SDL.git sdl
@@ -37,22 +39,14 @@ if "%~1" == "build-sdl2" (
     cmake --build build --config Release
 
     cd ..\..
-    copy /Y "vendor\sdl\build\Release\SDL2.dll" "output\SDL2.dll"
-    copy /Y "vendor\sdl\build\Release\SDL2.lib" "output\SDL2.lib"
-)
-
-if "%~1" == "build" (
-    odin build source -out:build\output\%EXE_NAME%
-)
-
-if "%~1" == "run" (
-    odin run source -out:build\output\%EXE_NAME%
-)
-
-if "%~1" == "build-debug" (
-    odin build source -out:build\output\%EXE_NAME% -debug
-)
-
-if "%~1" == "run-debug" (
-    odin run source -out:build\output\%EXE_NAME% -debug
+    copy /Y "%VENDOR_DIR%\sdl\build\Release\SDL2.dll" "%OUTPUT_DIR%\SDL2.dll"
+    copy /Y "%VENDOR_DIR%\sdl\build\Release\SDL2.lib" "%OUTPUT_DIR%\SDL2.lib"
+) else if "%~1" == "build" (
+    odin build source -out:%OUTPUT_DIR%\%EXE_NAME%
+) else if "%~1" == "run" (
+    odin run source -out:%OUTPUT_DIR%\%EXE_NAME%
+) else if "%~1" == "build-debug" (
+    odin build source -out:%OUTPUT_DIR%\%EXE_NAME% -debug
+) else if "%~1" == "run-debug" (
+    odin run source -out:%OUTPUT_DIR%\%EXE_NAME% -debug
 )
